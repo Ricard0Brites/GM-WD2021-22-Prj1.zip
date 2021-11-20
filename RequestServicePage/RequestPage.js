4619b318105c2dfbb5360e24caed925fd98afca7
Ricard0Brites <93977728+Ricard0Brites@users.noreply.github.com>
2021-11-20 22:09:59 +0000

    Update RequestPage.js

--- RequestServicePage/RequestPage.js
+++ RequestServicePage/RequestPage.js
@@ -1,45 +1,34 @@
-//applies a zooming animation when hovering
-const logoRef = document.querySelector('.Header-Logo');
-
-var hasEntered = false, hasLeft = false;
+//applies a zooming animation when hovering <-------------------------------------------------------> Header Logo <------------------------------------------------------------------------------------------------------------------->
+const logoRef = document.querySelector(".Header-Logo");
 
 logoRef.addEventListener("mouseleave", function () {
+    var transitionEndTrigger = GetTransitionEnd();
 
-    if( hasLeft == false)
-    {
-        logoRef.classList.add("resetLogo");
-        var computedStyle = window.getComputedStyle(logoRef), originalScaleX = computedStyle.getPropertyValue('width'), originalScaleY = computedStyle.getPropertyValue('Height');
-        logoRef.style.Width = originalScaleX;
-        logoRef.style.Height = originalScaleY;
+    logoRef.classList.remove("grow");    // removes the grow transition css properties
+    logoRef.classList.add("resetLogo"); // loads this classes(resetLogo) css properties
 
-    //resets the verifiers
-    hasLeft = true;
-    hasEntered = false;
-
-    //clears the queue
-        setTimeout(() => {
-           logoRef.classList.remove('resetLogo') ;
-           logoRef.classList.remove('grow');
-        }, 710);
-    }
-
-});
+    logoRef.addEventListener(transitionEndTrigger, function () {
+        logoRef.classList.remove('resetLogo');              // resets the css to repeat the process
+        logoRef.removeEventListener(transitionEndTrigger); // stops looking out for the end of the transition
+    }); // runs the function when the transition is complete (cross-browser)
+}); // removes the grow transition and adds the shrink followed by a css property reset
 logoRef.addEventListener("mouseenter", function () {
+    logoRef.classList.add("grow");
+}); //adds the css class named 'grow' to create a transition
 
-    if(!hasEntered)
-    {
-        logoRef.classList.add("grow");
 
-        var computedStyle = window.getComputedStyle(logoRef), originalScaleX = computedStyle.getPropertyValue('width'), originalScaleY = computedStyle.getPropertyValue('Height');
 
-        logoRef.style.Width = originalScaleX;
-        logoRef.style.Height = originalScaleY;
 
-        //ensures that there aren't multiple entrances
-        hasEntered = true;    
-        hasLeft = false;
-    } 
-});
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -56,6 +45,23 @@ function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
     do {
-      currentDate = Date.now();
+        currentDate = Date.now();
     } while (currentDate - date < milliseconds);
-  }
+} //interrupts code for X miliseconds
+
+//get transition end browser based
+function GetTransitionEnd() {
+    var transitions = {
+        //the different browsers' events
+        transition: "transitionend",
+        OTransition: "oTransitionEnd",
+        MozTransition: "trnasitionend",
+        WebkitTransform: "webkitTransitionEnd",
+    };
+    let bodyStyle = document.body.style;
+    for (let transition in transitions) {
+        if (bodyStyle[transition] != undefined) {
+            return transitions[transition];
+        }
+    }
+}
